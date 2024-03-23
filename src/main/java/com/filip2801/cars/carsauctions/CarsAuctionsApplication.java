07717a00d3f360e35d4278da7f3c65f67e4d2de2
Filip Karas <filip2801@gmail.com>
2024-03-23 01:33:38 +0100

    Configure project

---
+++ src/main/java/com/filip2801/cars/carsauctions/CarsAuctionsApplication.java
@@ -0,0 +1,13 @@
+package com.filip2801.cars.carsauctions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsAuctionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsAuctionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filip2801/cars/carsauctions/CarsAuctionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filip2801.cars.carsauctions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsAuctionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
